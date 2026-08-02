a9b0f1370857e6d0d0c14d05c0f4543573246675
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-02 15:32:11 +0530

    Initial commit - DatingApp UI

---
+++ src/app/app.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+  protected members=signal<any>([]);
+  private http = inject(HttpClient);
+  protected readonly title = signal('dating app');
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+}
+//  ngOnInit(): void {
+//     this.http.get('https://localhost:44329/api/members').subscribe({
+//      next:response=> this.members.set(response),
+//      error: error => console.log(error),
+//      complete:() => console.log('completed http request')
+//   })
+//  }
+
+
+
+async getMembers(){
+  try {
+     return lastValueFrom(this.http.get('https://localhost:44329/api/members'));
+    
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } 
+}
+}
