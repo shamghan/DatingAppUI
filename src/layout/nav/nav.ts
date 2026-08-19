1c0af0c40a9b070821b22c0c4e4b86ac91736b06
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-19 11:03:32 +0530

    login flow: add service and its schematics setting to ignore spec.ts file,

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
@@ -8,8 +9,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.css',
 })
 export class Nav {
+  private accountService= inject(AccountService)
     protected creds:any = {email:'', password:''};
     login() {
+      this.accountService.login(this.creds).subscribe({
+        next: result => console.log(result),
+        error: error => console.log(error)
+      })
       console.log(this.creds)
     }
 }
