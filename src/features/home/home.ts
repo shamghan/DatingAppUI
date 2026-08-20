6bf5923a4e12b9ef9ba11849abb2831cc94104d5
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-20 23:39:03 +0530

    display home page with register button

---
+++ src/features/home/home.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+  protected registerMode=signal(false);
+
+  showRegisterMode(){
+    this.registerMode.set(true);
+  }
+
+}
