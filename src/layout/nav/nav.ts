d3626f28c7568fced083ab41b0a42365f6b7c975
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-22 17:16:48 +0530

    routing in type script code  by injecting Router

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, Inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -10,12 +10,14 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './nav.css',
 })
 export class Nav {
-    protected accountService= inject(AccountService)
+    protected accountService= inject(AccountService);
+    private router = inject(Router);
     protected creds:any = {email:'', password:''};
 
     login() {
       this.accountService.login(this.creds).subscribe({
         next: result => {
+          this.router.navigateByUrl('/members');
           console.log(result);
 
           this.creds = {email:'', password:''};
@@ -26,5 +28,6 @@ export class Nav {
     }
     logout(){
       this.accountService.logout();
+      this.router.navigateByUrl('/');
     }
 }
