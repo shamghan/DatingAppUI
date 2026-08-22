9f6a5087c6fd8453681cb7b90b72928ba2f6f9e2
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-22 18:08:18 +0530

    create toast notification

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -2,6 +2,7 @@ import { Component, inject, Inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -13,16 +14,19 @@ export class Nav {
     protected accountService= inject(AccountService);
     private router = inject(Router);
     protected creds:any = {email:'', password:''};
-
+    private toast = inject(ToastService)
     login() {
       this.accountService.login(this.creds).subscribe({
         next: result => {
           this.router.navigateByUrl('/members');
           console.log(result);
-
+          this.toast.success('Logged in successfull');
           this.creds = {email:'', password:''};
         },
-        error: error => console.log(error)
+        error: error => {
+         this.toast.error(error.error);
+          console.log(error.error);
+        }
       })
       console.log(this.creds)
     }
