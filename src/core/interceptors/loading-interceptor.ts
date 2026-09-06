bdd5a928238f9fbafde44ff1fb520bf576bf2d2c
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-09-06 18:19:31 +0530

    chachind data

--- src/core/interceptors/loading-interceptor.ts
+++ src/core/interceptors/loading-interceptor.ts
@@ -1,15 +1,29 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
 import { BusyService } from '../services/busy-service';
 import { inject } from '@angular/core';
 import { finalize } from 'rxjs/internal/operators/finalize';
 
-import { delay } from 'rxjs';
-
+import { delay, of, tap } from 'rxjs';
+const cache = new Map<string, HttpEvent<unknown>>();
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  
   const busyService = inject(BusyService);
+  
+  if(req.method === 'GET')
+  {
+    const cacheResponse = cache.get(req.url);
+    if(cacheResponse)
+    {
+      return of(cacheResponse);
+    }
+  }
+  
   busyService.busy();
   return next(req).pipe(
-    delay(500),
+    delay(2000),
+    tap(response=>{
+      cache.set(req.url,response);
+    }),
     finalize(() => {
       busyService.idle();
     })
