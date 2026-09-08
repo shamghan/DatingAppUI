9e5f5f9721b0241d3f0a0e05854e3cfd212227c4
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-09-09 00:16:24 +0530

    create reactive form with validation

--- src/features/account/register/register.ts
+++ src/features/account/register/register.ts
@@ -1,26 +1,58 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, inject, input, OnInit, output } from '@angular/core';
 import { RegisterCreds, User } from '../../../type/user';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit {
+  
   private accountService=inject(AccountService);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup= new FormGroup({});
+
+  ngOnInit(): void {
+     this.initializeForm();
+  }
+  initializeForm()
+  {
+    this.registerForm = new FormGroup({
+      email: new FormControl('johndoe@test.com', [Validators.required, Validators.email]),
+      displayName: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe(()=>{
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string):ValidatorFn
+  {
+    return (control: AbstractControl): ValidationErrors | null=>{
+      const parent =control.parent;
+      if(!parent) return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null :{passwordMismatch:true}
+    }
+  }
   register() {
-      this.accountService.registerUser(this.creds).subscribe({
-        next: response => {
-          console.log(response);
-          this.cancel();
-        },
-        error: error=>console.log(error)
-      })
+    console.log(this.registerForm.value);
+
+      // this.accountService.registerUser(this.creds).subscribe({
+      //   next: response => {
+      //     console.log(response);
+      //     this.cancel();
+      //   },
+      //   error: error=>console.log(error)
+      // })
   }
   cancel() {
    this.cancelRegister.emit(false);
