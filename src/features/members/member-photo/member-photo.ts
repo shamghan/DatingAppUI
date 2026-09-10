d66f13c5dc46c14819572c0ebbdd28891149d21f
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-09-10 08:30:33 +0530

    when uploading first image then set main photo also after phot upload

--- src/features/members/member-photo/member-photo.ts
+++ src/features/members/member-photo/member-photo.ts
@@ -45,6 +45,9 @@ export class MemberPhoto implements OnInit {
       this.memberService.editMode.set(false);
       this.loading.set(false);
       this.photos.update(photos=> [...photos, photo])
+      if(!this.memberService.member()?.imageUrl){
+        this.setMainLocalPhoto(photo);
+      }
     },
     error: error=>{
       console.log('Error uploading image', error);
@@ -58,14 +61,7 @@ export class MemberPhoto implements OnInit {
  {
   this.memberService.setMainPhoto(photo).subscribe({
     next: ()=>{
-      const currentUser= this.accountService.currentUser();
-      if(currentUser) 
-        currentUser.imageUrl= photo.url;
-      this.accountService.setCurrentUser(currentUser as User);
-      this.memberService.member.update(member =>({
-        ...member,
-        imageUrl: photo.url
-      }) as Member)
+    this.setMainLocalPhoto(photo);
     }
   });
  }
@@ -76,5 +72,15 @@ export class MemberPhoto implements OnInit {
       this.photos.update(photos=> photos.filter(x=>x.id !== photoId))
     }
   });
+ }
+ private setMainLocalPhoto(photo:Photo){
+  const currentUser= this.accountService.currentUser();
+      if(currentUser) 
+        currentUser.imageUrl= photo.url;
+      this.accountService.setCurrentUser(currentUser as User);
+      this.memberService.member.update(member =>({
+        ...member,
+        imageUrl: photo.url
+      }) as Member)
  }
 }
